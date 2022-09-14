719ea467fa5e64e7680dfd35365eb85b8b83fa55
Yali Wang <ywang35@alaska.edu>
2022-09-13 21:03:39 -0800

    Upload the Vue frontend

---
+++ mlt-frontend/src/App.vue
@@ -0,0 +1,21 @@
+<template src="./index.html">
+</template>
+
+<script>
+    import axios from "axios"
+    export default {
+        name: 'App',
+        methods: {
+            async trigger() {
+                try {
+                    let res = await axios.get("http://localhost:8888")
+                    alert(res.data.message)
+                } catch (e) {
+                    console.log(e)
+                }
+            }
+        }
+    }
+</script>
+
+<style src="./style.css"></style>
