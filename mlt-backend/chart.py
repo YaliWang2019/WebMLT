9658a3b0084d0827e933e1e697dd76a60f4f9005
Yali Wang <ywang35@alaska.edu>
2022-10-05 14:24:40 -0800

    Trying to call function from chart.py inside server.py

--- mlt-backend/chart.py
+++ mlt-backend/chart.py
@@ -2,22 +2,7 @@ import matplotlib.pyplot as plt
 from tabulate import tabulate
 import numpy as np
 
-# chart
-def simple_chart(a):
-    x = np.linspace(0,5,10)
-    y = x**2
-    plt.plot(x, y)
-    int(a)
-    result = np.interp(a, x,y)
-    print(result)
-
-# table
-data = [[1, "apple"], 
-        [2, "banana"], 
-        [3, "orange"], 
-        [4, "cherry"]]
-
-col_names = ["ID", "Item"]
-  
-#display table
-tabulate(data, headers=col_names, tablefmt="fancy_grid", showindex="always")
+def simple_chart(x):
+    x2 = np.square(x)
+    y = pow(x, 3)
+    return x2, y
--- mlt-frontend/src/App.vue
+++ mlt-frontend/src/App.vue
@@ -3,41 +3,23 @@
 
 <script>
     import axios from "axios"
-//    import { defineComponent } from 'vue'
     export default {
         name: 'App',
         methods: {
-            async trigger() {
-                try {
-                    let res = await axios.get("http://localhost:5000")
-                    alert(res.data.message)
-                } catch (e) {
-                    console.log(e)
-                }
-            },
-            async scipy(obj) {
+            async scipy() {
                 try {
                     let res = await axios.post("http://localhost:5000", {
                         //The 'obj' object accepted by the function is a submission event, 
                         //the 'target' refers to the form object that initiated this submission event.
                         //It includes the array of the form that the user submitted
-                        "name": obj.target[0].value
+                        "name": this.xValue
                     })
                     alert(res.data.messages)
                 } catch (e) {
                     console.log(e)
                 }
             },
-            data() {
-                return {
-                    textFieldValue: ''
-                }
-            },
-            getName() {
-                axios.get("https://reqres.in/api/users/2").then(response => {
-                    this.textFieldValue = response.data.data.x
-                })
-            }
+
         },
         
     }
