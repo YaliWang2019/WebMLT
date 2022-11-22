b9dec15cfdf46303b8c1aaf763144d4f50e2bd83
Yali Wang <ywang35@alaska.edu>
2022-11-21 23:29:46 -0900

    css style doesnt apply

---
+++ mlt-frontend/src/views/LRView/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="LR">
+    <h1>This is an about page, supposed to be the linear regression model page.</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: 'LRView',
+}
+</script>
