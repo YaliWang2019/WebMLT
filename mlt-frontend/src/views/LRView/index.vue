43b74ae1be7b64e0137b736b9fd3627f35562b8d
Yali Wang <ywang35@alaska.edu>
2022-12-05 17:59:04 -0900

    some progress

--- mlt-frontend/src/views/LRView/index.vue
+++ mlt-frontend/src/views/LRView/index.vue
@@ -56,15 +56,10 @@
             <h2 align=left></h2>
             <em>Please select the parameters you want to use: </em>
             <form @submit.prevent="dataPreprocess">
-                <select name="languages" id="lang">
-            <option value="javascript">JavaScript</option>
-            <option value="php">PHP</option>
-            <option value="java">Java</option>
-            <option value="golang">Golang</option>
-            <option value="python">Python</option>
-            <option value="c#">C#</option>
-            <option value="C++">C++</option>
-            <option value="erlang">Erlang</option>
+            <select name="parameters" id="para">
+            <option value="testsize">test_size</option>
+            <option value="randomrate">random_rate</option>
+            
             </select>
                 <input type="submit" value="Submit" />
             </form>
