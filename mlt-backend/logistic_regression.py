5915d960114c08bb9e2942f4eb355fd14ca9a047
Yali Wang <ywang35@alaska.edu>
2022-12-12 01:57:13 -0900

    working on logistic regression model

--- mlt-backend/logistic_regression.py
+++ mlt-backend/logistic_regression.py
@@ -12,7 +12,7 @@ import base64
 import itertools
 from sklearn.model_selection import train_test_split
 from matplotlib.pyplot import figure
-from sklearn.linear_model import SGDRegressor
+from sklearn.linear_model import LogisticRegression
 from sklearn import metrics
 from sklearn.metrics import confusion_matrix
 from http.server import HTTPServer, BaseHTTPRequestHandler, SimpleHTTPRequestHandler
@@ -25,7 +25,7 @@ csv_file = {}
 X_database = {}
 Y_database = {}
 # Phase (1). data collection (file upload):
-def lgrFileUpload(files):
+def lgr_fileUpload(files):
   #  if request.method == 'POST':
   if files['file'].content_type != 'text/csv':
     return (json.dumps({'message': 'File must be a CSV'}), 400)
@@ -39,13 +39,13 @@ def lgrFileUpload(files):
   return (json.dumps({'id': uuid}), 200)
 
 # Phase (2). data preprocessing (removing missing values and scaling, return processed dataframe preview): 
-def lgrRmMissingvalues(id):
+def lgr_rmMissingvalues(id):
   df = csv_file[id]
   df_new = df.dropna()
   df_preview = df_new[0:5]
   return ((df_preview.to_json()), 200)
 
-def lgrScaling(id, scaleMode):
+def lgr_scaling(id, scaleMode):
   df = csv_file[id]
   df_new = df.dropna()
   X = df.iloc[:, 2:]
@@ -58,78 +58,71 @@ def lgrScaling(id, scaleMode):
   y = np.where(y=='M', 0, 1)
   if scaleMode == "standardization":
     # standardization
-    Y = (Y - np.mean(Y)) / np.std(Y)
+    X = (X - np.mean(X)) / np.std(X)
   elif scaleMode == "normalization":
     # normalization
-    Y = (Y - np.min(Y)) / (np.max(Y) - np.min(Y))
+    X = (X - np.min(X)) / (np.max(X) - np.min(X))
   # error catching logic required here
-  return (X, Y, y)
+  return (X, y)
 
 # Phase 3: data visualization (whole data visualization, training data visualization, and testing data visualization, return charts)
 
-def lgrScatterImg(id, scaleMode):
-  (X, Y, y) = lgrScaling(id, scaleMode)
+def lgr_scatterImg(id, scaleMode):
+  (X, y) = lgr_scaling(id, scaleMode)
   plt.clf()
   figure(figsize=(8, 6), dpi=80)
-  plt.scatter(X, Y)
+  plt.scatter(X, y)
   
   plt.title("Visualize the full Dataset")
   plt.xlabel('X')
-  plt.ylabel('Y')
+  plt.ylabel('y')
 
-  return (json.dumps({'lgrImgScatter':img_to_base64(plt)}), 200)
+  return (json.dumps({'lgrImgScatter':lgr_img_to_base64(plt)}), 200)
 
 
   # Split the Dataset into Training and Test Set
-def lgrSpliting(id, test_size=0.2, random_state=0, scaleMode="normalization"):
-  (X, Y) = lgrScaling(id, scaleMode)
-  X_train_split, X_test_split, Y_train, Y_test = train_test_split(X, Y, test_size=test_size, random_state=random_state)
-  return (X_train_split, X_test_split, Y_train, Y_test)
+def lgr_spliting(id, test_size=0.2, random_state=0, scaleMode="normalization"):
+  (X, y) = lgr_scaling(id, scaleMode)
+  X_train_split, X_test_split, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=random_state)
+  return (X_train_split, X_test_split, y_train, y_test)
 
 def lgr_train_test_imgs(id, test_size, random_state):
-  (X_train_split, X_test_split, Y_train, Y_test) = lgrSpliting(id, float(test_size), int(random_state))
+  (X_train_split, X_test_split, y_train, y_test) = lgr_spliting(id, float(test_size), int(random_state))
   plt.clf()
   figure(figsize=(15, 6), dpi=80)
   plt.subplot(1, 2, 1) # row 1, col 2 index 1
-  plt.scatter(X_train_split, Y_train)
+  plt.scatter(X_train_split, y_train)
   # trainImg = img_to_base64(plt)
   plt.title("Training Data")
   plt.xlabel('X_train')
   plt.ylabel('Y_train')
 
   plt.subplot(1, 2, 2) # index 2
-  plt.scatter(X_test_split, Y_test)
+  plt.scatter(X_test_split, y_test)
   plt.title("Testing Data")
   plt.xlabel('X_test')
   plt.ylabel('Y_test')
   plt.tight_layout()
-  trainTestestImg = img_to_base64(plt)
+  trainTestestImg = lgr_img_to_base64(plt)
 
   return (json.dumps({'trainTestestImg': trainTestestImg}), 200)
 
 # Phase 4: model training
 # proving X_train, X_test, regressor, and Y_pred
 def lgr_pre_train(id, test_size, random_state):
-  (X_train_split, X_test_split, Y_train, Y_test) = lgrSpliting(id, test_size, random_state)
+  (X_train, X_test, y_train, y_test) = lgr_spliting(id, test_size, random_state)
   # Create a 2D array for training and test data to make it compatible with
   # scikit-learn (This is specific to scikit-learn because of the way it accepts input data)
-  X_train = X_train_split.reshape(-1, 1)
-  X_test = X_test_split.reshape(-1, 1)
+  # Create Logistic Regression object
+  clf = LogisticRegression()
 
-  # Initialize Model
-
-  regressor = SGDRegressor()
-
-  # Run Model Training
-  regressor.fit(X_train, Y_train)
-
-  # Predict on the Test Data
-  Y_pred = regressor.predict(X_test)
+# Train Logistic Regression Classifer
+  clf.fit(X_train, y_train)
 
   return (X_train, X_test, X_train_split, X_test_split, regressor, Y_train, Y_test, Y_pred)
 
   # plt.tight_layout()
-def lgrModelTraining(id, test_size, random_state):
+def lgr_modelTraining(id, test_size, random_state):
   (_, _, _, X_test_split, _, _, Y_test, Y_pred) = lgr_pre_train(id, test_size, random_state)
 
   # Plot the predictions and the original test data
@@ -142,7 +135,7 @@ def lgrModelTraining(id, test_size, random_state):
   
   plt.legend(loc='best')
 
-  return (json.dumps({'imgPrediction':img_to_base64(plt)}), 200)
+  return (json.dumps({'imgPrediction':lgr_img_to_base64(plt)}), 200)
 
 # Phase 5: accuracy
 def lgr_accuracy(id, test_size, random_state):
@@ -153,7 +146,7 @@ def lgr_accuracy(id, test_size, random_state):
   # Evaluate the quality of the training (Generate Evaluation Metrics)
   return (json.dumps({'Mean Absolute Error:': meanAbErr, 'Mean Squared Error:': meanSqErr, 'Root Mean Squared Error:': rootMeanSqErr}), 200)
 
-def lgrConfusionMatrix(cm, classes, normalize=False, title='Confusion matrix', cmap=plt.cm.Blues):
+def lgr_confusionMatrix(cm, classes, normalize=False, title='Confusion matrix', cmap=plt.cm.Blues):
   if normalize:
       cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
 
@@ -172,17 +165,17 @@ def lgrConfusionMatrix(cm, classes, normalize=False, title='Confusion matrix', c
   plt.tight_layout()
   plt.ylabel('True label')
   plt.xlabel('Predicted label')
-  return (img_to_base64(plt))
+  return (lgr_img_to_base64(plt))
 
-def lgrMakeConfusionMatrix(id, test_size, random_state):
+def lgr_makeConfusionMatrix(id, test_size, random_state):
   (_, _, _, _, _, _, Y_test, Y_pred) = lgr_pre_train(id, float(test_size), int(random_state))
   cm = confusion_matrix(Y_test, Y_pred)
   labels = ["+", "-"]
-  matrix = lgrConfusionMatrix(cm, labels)  
+  matrix = lgr_confusionMatrix(cm, labels)  
   return (json.dumps({'confsMatrix': matrix}), 200)
 
 
-def img_to_base64(plt):
+def lgr_img_to_base64(plt):
   chart = BytesIO()
   plt.savefig(chart, format = 'png')
   chart.seek(0)
