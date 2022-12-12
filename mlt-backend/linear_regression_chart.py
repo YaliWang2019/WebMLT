59a836a5f676f572cfde67c2d824477ce1c7132f
Yali Wang <ywang35@alaska.edu>
2022-12-11 17:38:13 -0900

    updated progress

--- mlt-backend/linear_regression_chart.py
+++ mlt-backend/linear_regression_chart.py
@@ -147,33 +147,6 @@ def accuracy(id, test_size, random_state):
   # Evaluate the quality of the training (Generate Evaluation Metrics)
   return (json.dumps({'Mean Absolute Error:': meanAbErr, 'Mean Squared Error:': meanSqErr, 'Root Mean Squared Error:': rootMeanSqErr}), 200)
 
-def confusionMatrix(cm, classes, normalize=False, title='Confusion matrix', cmap=plt.cm.Blues):
-  if normalize:
-      cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-
-  plt.imshow(cm, interpolation='nearest', cmap=cmap)
-  plt.title(title)
-  plt.colorbar()
-  tick_marks = np.arange(len(classes))
-  plt.xticks(tick_marks, classes, rotation=45)
-  plt.yticks(tick_marks, classes)
-
-  fmt = '.2f' if normalize else 'd'
-  thresh = cm.max() / 2.
-  for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-    plt.text(j, i, format(cm[i, j], fmt), horizontalalignment="center", color="white" if cm[i, j] > thresh else "black")
-
-  plt.tight_layout()
-  plt.ylabel('True label')
-  plt.xlabel('Predicted label')
-  return (img_to_base64(plt))
-
-def makeConfusionMatrix(id, test_size, random_state):
-  (_, _, _, _, _, _, Y_test, Y_pred) = pre_train(id, float(test_size), int(random_state))
-  cm = confusion_matrix(Y_test, Y_pred)
-  labels = ["+", "-"]
-  matrix = confusionMatrix(cm, labels)  
-  return (json.dumps({'confsMatrix': matrix}), 200)
 
 def generate_dataset():
   X = np.linspace(0, 2, 100)
